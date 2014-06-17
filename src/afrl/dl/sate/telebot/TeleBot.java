72cd57c90aa8ad063f98ce6f1475a0e55a096ebe
cole <cole@cole-HP-dv6>
2014-06-17 09:11:51 -0400

    First commit!

---
+++ src/afrl/dl/sate/telebot/TeleBot.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package afrl.dl.sate.telebot;
+
+/**
+ *
+ * @author cole
+ */
+public class TeleBot {
+
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hooray!");
+        
+        
+    }//End main
+    
+    
+    
+}//End class TeleBot
